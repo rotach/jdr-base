68b7b6db818627bfae41558fbd374e53860f54a9
John Rotach <john@2lemetry.com>
2015-02-02 14:52:43 -0700

    Removed unneeded coobook dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,6 @@ description      'Installs/Configures jdr-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'apt',             "= 2.3.4"
-depends          'build-essential', "= 2.0.4"
-depends          'magic_shell',     "= 0.3.2"
 depends          'ntp',             "= 1.5.4"
 depends          'python',          "= 1.4.4"
-depends          'screen',          "= 0.8.0"
 depends          'vim',             "= 1.1.2"
